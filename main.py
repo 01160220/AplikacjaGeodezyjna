c4512af84e83a46e9124e83761e3c2aa3ae68725
01160220@pw.edu.pl <01160220@pw.edu.pl>
2022-05-31 02:08:11 +0200

    31.05.2022_02:07

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ import sys
 from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
 from PyQt5.QtGui import QIcon
 from ap_interfejs import *
-import math
+import math as m
 from math import sqrt, radians
 import numpy as np
 
 class MyApp(QDialog):
     def __init__(self):
         super().__init__()
+        self.setStyleSheet("background-color: #8fd970;")
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowIcon(QIcon('ikonka.png'))
         self.setWindowIcon(QtGui.QIcon('ikonka.png'))
-        self.ui.tabWidget_2.currentChanged.connect(self.zmiana_karty)
-        self.ui.tabWidget.currentChanged.connect(self.zmiana_karty)
         
         self.ui.przeliczenie92.clicked.connect(self.obsluga_1992)
         self.ui.przeliczenie00_2.clicked.connect(self.obsluga_2000)
@@ -29,60 +28,21 @@ class MyApp(QDialog):
         self.ui.przeliczenie92_2.clicked.connect(self.obsluga_1992_SMS)
         self.ui.przeliczenie00_3.clicked.connect(self.obsluga_2000_SMS)
         self.ui.przeliczenieXYZ_2.clicked.connect(self.obsluga_XYZ_SMS)
+        
+        self.ui.przeliczenie92_4.clicked.connect(self.obsluga_1992_grad)
+        self.ui.przeliczenie00_5.clicked.connect(self.obsluga_2000_grad)
+        self.ui.przeliczenieXYZ_4.clicked.connect(self.obsluga_XYZ_grad)
+        
+        self.ui.przeliczenie92_5.clicked.connect(self.obsluga_1992_rad)
+        self.ui.przeliczenie00_6.clicked.connect(self.obsluga_2000_rad)
+        self.ui.przeliczenieXYZ_5.clicked.connect(self.obsluga_XYZ_rad)
         self.show()
-        
-    def zmiana_karty(self):
-        if self.ui.tabWidget_2.currentIndex() == 0:  
-            if  self.ui.tabWidget.currentIndex() == 0:
-                print("S_M_S")
-            else:
-                print("Dziesietne")
-        else:
-            print('Grady')
-            
+
     def wybor_elipsoidy(self):
-        if self.ui.radioButton.isChecked():
+        if self.ui.radioButton.isChecked() or self.ui.radioButton_10.isChecked() or self.ui.radioButton_20.isChecked() or self.ui.radioButton_25.isChecked():
             self.a = 6378137.0
             self.b = 6356752.31414036
-        elif self.ui.wgs_2.isChecked():
-            self.a = 6378137.0 
-            self.b = 6356752.31424518 
-        else:
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Critical)
-            msg.setWindowIcon(QtGui.QIcon('wykrzyknik.png'))
-            msg.setText('Wybierz elipsoide!')
-            msg.setInformativeText
-            msg.setWindowTitle("Sprawdź wprowadzone dane!")
-            msg.exec_()
-        self.flattening = (self.a - self.b) / self.a
-        self.ecc2 = (2 * self.flattening - self.flattening ** 2)
-        
-    def wybor_elipsoidy_SMS(self):
-        if self.ui.radioButton_10.isChecked():
-            self.a = 6378137.0
-            self.b = 6356752.31414036
-        elif self.ui.wgs_3.isChecked():
-            self.a = 6378137.0 
-            self.b = 6356752.31424518 
-        else:
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Critical)
-            msg.setWindowIcon(QtGui.QIcon('wykrzyknik.png'))
-            msg.setText('Wybierz elipsoide!')
-            msg.setInformativeText
-            msg.setWindowTitle("Sprawdź wprowadzone dane!")
-            msg.exec_()
-        self.flattening = (self.a - self.b) / self.a
-        self.ecc2 = (2 * self.flattening - self.flattening ** 2)
-        self.flattening = (self.a - self.b) / self.a
-        self.ecc2 = (2 * self.flattening - self.flattening ** 2)
-        
-    def wybor_elipsoidy_grad(self):
-        if self.ui.radioButton.isChecked():
-            self.a = 6378137.0
-            self.b = 6356752.31414036
-        elif self.ui.wgs_2.isChecked():
+        elif self.ui.wgs_2.isChecked() or self.ui.wgs_3.isChecked() or self.ui.wgs_6.isChecked() or self.ui.wgs_5.isChecked():
             self.a = 6378137.0 
             self.b = 6356752.31424518 
         else:
@@ -98,7 +58,8 @@ class MyApp(QDialog):
         
     def obsluga_1992(self):
         self.wybor_elipsoidy()
-        self.wprowadzanie_danych()
+        zmienna = 'stopnie'
+        self.wprowadzanie_danych(zmienna)
         self.fi = radians(self.fi)
         self.lam = radians(self.lam)
         self.xy1992()
@@ -108,7 +69,8 @@ class MyApp(QDialog):
         
     def obsluga_2000(self):
         self.wybor_elipsoidy()
-        self.wprowadzanie_danych()
+        zmienna = 'stopnie'
+        self.wprowadzanie_danych(zmienna)
         self.fi = radians(self.fi)
         self.lam = radians(self.lam)
         self.xy2000_strefy()
@@ -118,46 +80,17 @@ class MyApp(QDialog):
         
     def obsluga_XYZ(self):
         self.wybor_elipsoidy()
-        self.wprowadzanie_danych()   
+        zmienna = 'stopnie'
+        self.wprowadzanie_danych(zmienna)   
         self.fi = radians(self.fi)
         self.lam = radians(self.lam)
-        self.XYZ()
-        self.ui.label.setText('X =' + str(self.X) + ' ' + '[m]')
-        self.ui.label_2.setText('Y =' + str(self.Y) + ' ' + '[m]')
-        self.ui.label_3.setText('Z =' + str(self.Z) + ' ' + '[m]')
-    
-    def obsluga_1992_grad(self):
-        self.wybor_elipsoidy()
-        self.wprowadzanie_danych()
-        self.fi = radians(self.fi)
-        self.lam = radians(self.lam)
-        self.xy1992()
-        self.ui.label.setText('x1992 =' + str(self.x1992) + ' ' + '[m]')
-        self.ui.label_2.setText('y1992 =' + str(self.y1992) + ' ' + '[m]')
-        self.ui.label_3.setText('')
-        
-    def obsluga_2000_grad(self):
-        self.wybor_elipsoidy()
-        self.wprowadzanie_danych()
-        self.fi = radians(self.fi)
-        self.lam = radians(self.lam)
-        self.xy2000_strefy()
-        self.ui.label.setText('x1992 =' + str(self.x2000) + ' ' + '[m]')
-        self.ui.label_2.setText('y1992 =' + str(self.y2000) + ' ' + '[m]')
-        self.ui.label_3.setText('')
-        
-    def obsluga_XYZ_grad(self):
-        self.wybor_elipsoidy_grad()
-        self.wprowadzanie_danych()   
-        self.fi = radians(self.fi)
-        self.lam = radians(self.lam)
-        self.XYZ()
+        self.XYZ(zmienna)
         self.ui.label.setText('X =' + str(self.X) + ' ' + '[m]')
         self.ui.label_2.setText('Y =' + str(self.Y) + ' ' + '[m]')
         self.ui.label_3.setText('Z =' + str(self.Z) + ' ' + '[m]')
     
     def obsluga_1992_SMS(self):
-        self.wybor_elipsoidy_SMS()
+        self.wybor_elipsoidy()
         self.wprowadzanie_SMS()    
         self.xy1992()
         self.ui.label_4.setText('x1992 =' + str(self.x1992) + ' ' + '[m]')
@@ -165,7 +98,7 @@ class MyApp(QDialog):
         self.ui.label_8.setText('')
         
     def obsluga_2000_SMS(self):
-        self.wybor_elipsoidy_SMS()
+        self.wybor_elipsoidy()
         self.wprowadzanie_SMS()    
         self.xy2000_strefy_SMS()
         self.ui.label_4.setText('x1992 =' + str(self.x2000) + ' ' + '[m]')
@@ -173,17 +106,109 @@ class MyApp(QDialog):
         self.ui.label_8.setText('')
         
     def obsluga_XYZ_SMS(self):
-        self.wybor_elipsoidy_SMS()
+        self.wybor_elipsoidy()
         self.wprowadzanie_SMS()   
         self.XYZ_SMS()
         self.ui.label_4.setText('X =' + str(self.X) + ' ' + '[m]')
         self.ui.label_7.setText('Y =' + str(self.Y) + ' ' + '[m]')
         self.ui.label_8.setText('Z =' + str(self.Z) + ' ' + '[m]')
+    
+    
+    def obsluga_1992_grad(self):
+        self.wybor_elipsoidy()
+        zmienna = 'grady'
+        self.wprowadzanie_danych(zmienna) 
+        self.fi = self.fi*np.pi/200
+        self.lam = self.lam*np.pi/200
+        self.xy1992()
+        self.ui.label_31.setText('x1992 =' + str(self.x1992) + ' ' + '[m]')
+        self.ui.label_32.setText('y1992 =' + str(self.y1992) + ' ' + '[m]')
+        self.ui.label_33.setText('')
+        
+    def obsluga_2000_grad(self):
+        self.wybor_elipsoidy()
+        zmienna = 'grady'
+        self.wprowadzanie_danych(zmienna) 
+        self.fi = self.fi*np.pi/200
+        self.lam = self.lam*np.pi/200
+        self.xy2000_strefy()
+        self.ui.label_31.setText('x1992 =' + str(self.x2000) + ' ' + '[m]')
+        self.ui.label_32.setText('y1992 =' + str(self.y2000) + ' ' + '[m]')
+        self.ui.label_33.setText('')
+        
+    def obsluga_XYZ_grad(self):
+        self.wybor_elipsoidy()
+        zmienna = 'grady'
+        self.wprowadzanie_danych(zmienna)   
+        self.fi = self.fi*np.pi/200
+        self.lam = self.lam*np.pi/200
+        self.XYZ(zmienna)
+        self.ui.label_31.setText('X =' + str(self.X) + ' ' + '[m]')
+        self.ui.label_32.setText('Y =' + str(self.Y) + ' ' + '[m]')
+        self.ui.label_33.setText('Z =' + str(self.Z) + ' ' + '[m]')
+    
+     
+    def obsluga_1992_rad(self):
+        self.wybor_elipsoidy()
+        zmienna = 'radiany'
+        self.wprowadzanie_danych(zmienna) 
+        self.fi = radians(self.fi)
+        self.lam = radians(self.lam)
+        self.xy1992()
+        self.ui.label_39.setText('x1992 =' + str(self.x1992) + ' ' + '[m]')
+        self.ui.label_40.setText('y1992 =' + str(self.y1992) + ' ' + '[m]')
+        self.ui.label_41.setText('')
+        
+    def obsluga_2000_rad(self):
+        self.wybor_elipsoidy()
+        zmienna = 'radiany'
+        self.wprowadzanie_danych(zmienna) 
+        self.fi = radians(self.fi)
+        self.lam = radians(self.lam)
+        self.xy2000_strefy()
+        self.ui.label_39.setText('x1992 =' + str(self.x2000) + ' ' + '[m]')
+        self.ui.label_40.setText('y1992 =' + str(self.y2000) + ' ' + '[m]')
+        self.ui.label_41.setText('')
+        
+    def obsluga_XYZ_rad(self):
+        self.wybor_elipsoidy()
+        zmienna = 'radiany'
+        self.wprowadzanie_danych(zmienna)   
+        self.fi = radians(self.fi)
+        self.lam = radians(self.lam)
+        self.XYZ(zmienna)
+        self.ui.label_39.setText('X =' + str(self.X) + ' ' + '[m]')
+        self.ui.label_40.setText('Y =' + str(self.Y) + ' ' + '[m]')
+        self.ui.label_41.setText('Z =' + str(self.Z) + ' ' + '[m]')
 
-    def wprowadzanie_danych(self):
-        if len(self.ui.fi_wprow_2.text()) != 0 and float(self.ui.fi_wprow_2.text()) >= 49 and float(self.ui.fi_wprow_2.text()) <= 54.8333: # zakres na phi <49 stopni, 54 stopni 50 minut>
-            self.fi = float(self.ui.fi_wprow_2.text())
-        elif len(self.ui.fi_wprow_2.text()) == 0:
+
+    def wprowadzanie_danych(self,zmienna):
+        if zmienna == 'stopnie':
+            dlugosc_fi = len(self.ui.fi_wprow_2.text())
+            wartosc_fi = float(self.ui.fi_wprow_2.text())
+            dol_fi = 49
+            gora_fi = 54.8333
+            dol_lam = 14.1167
+            gora_lam = 24.15
+        elif zmienna == 'grady':
+            dlugosc_fi = len(self.ui.fi_wprow_9.text())
+            wartosc_fi = float(self.ui.fi_wprow_9.text())
+            dol_fi = 54.4444
+            gora_fi = 60.9259
+            dol_lam = 15.6852
+            gora_lam = 26.8333
+        elif zmienna == 'radiany':
+            dlugosc_fi = len(self.ui.fi_wprow_10.text())
+            wartosc_fi = float(self.ui.fi_wprow_10.text())
+            dol_fi = 0.855211
+            gora_fi = 0.957022
+            dol_lam = 0.246383
+            gora_lam = 0.421497
+        
+    
+        if dlugosc_fi != 0 and wartosc_fi >= dol_fi and wartosc_fi <= gora_fi: # zakres na phi <49 stopni, 54 stopni 50 minut>
+            self.fi = wartosc_fi
+        elif dlugosc_fi == 0:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Critical)
             msg.setWindowIcon(QtGui.QIcon('wykrzyknik.png'))
@@ -199,9 +224,21 @@ class MyApp(QDialog):
             msg.setInformativeText
             msg.setWindowTitle("Sprawdź wprowadzone dane!")
             msg.exec_()
-        if len(self.ui.lambda_wprow_2.text()) != 0 and float(self.ui.lambda_wprow_2.text()) >= 14.1167 and float(self.ui.lambda_wprow_2.text()) <= 24.15: # zakres na lambda <14 stopni 07 minut, 24 stopnie 09 minut>
-            self.lam = float(self.ui.lambda_wprow_2.text())
-        elif len(self.ui.lambda_wprow_2.text()) == 0:
+        
+        
+        if zmienna == 'stopnie':
+            dlugosc_lam = len(self.ui.lambda_wprow_2.text())
+            wartosc_fi = float(self.ui.lambda_wprow_2.text())
+        if zmienna == 'grady':
+            dlugosc_lam = len(self.ui.lambda_wprow_5.text())
+            wartosc_fi = float(self.ui.lambda_wprow_5.text())
+        if zmienna == 'radiany':
+            dlugosc_lam = len(self.ui.lambda_wprow_6.text())
+            wartosc_fi = float(self.ui.lambda_wprow_6.text())
+            
+        if dlugosc_lam != 0 and wartosc_fi >= dol_lam and wartosc_fi <= gora_lam: # zakres na lambda <14 stopni 07 minut, 24 stopnie 09 minut>
+            self.lam = wartosc_fi
+        elif dlugosc_lam == 0:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Critical)
             msg.setWindowIcon(QtGui.QIcon('wykrzyknik.png'))
@@ -338,22 +375,22 @@ class MyApp(QDialog):
         self.y1992 = round((ygk * m0) + 500000, 5)
         
     def xy2000_strefy(self):
-        if self.ui.radioButton_2.isChecked():
+        if self.ui.radioButton_2.isChecked() or self.ui.radioButton_16.isChecked() or self.ui.radioButton_21.isChecked():
             self.strefa_00 = 5
             self.lam_0 = 15
             self.xy2000()
             
-        elif self.ui.radioButton_3.isChecked():
+        elif self.ui.radioButton_3.isChecked() or self.ui.radioButton_17.isChecked() or self.ui.radioButton_22.isChecked():
             self.strefa_00 = 6
             self.lam_0 = 18
             self.xy2000()
             
-        elif self.ui.radioButton_4.isChecked():
+        elif self.ui.radioButton_4.isChecked() or self.ui.radioButton_18.isChecked() or self.ui.radioButton_23.isChecked():
             self.strefa_00 = 7
             self.lam_0 = 21
             self.xy2000()
             
-        elif self.ui.radioButton_5.isChecked():
+        elif self.ui.radioButton_5.isChecked() or self.ui.radioButton_19.isChecked() or self.ui.radioButton_24.isChecked():
             self.strefa_00 = 8
             self.lam_0 = 24
             self.xy2000()
@@ -366,7 +403,7 @@ class MyApp(QDialog):
             msg.setWindowTitle("Sprawdź wprowadzone dane!")
             msg.exec_()
             
-    def xy2000_strefy_SMS(self):
+    def xy2000_strefy_SMS(self):        
         if self.ui.radioButton_6.isChecked():
             self.strefa_00 = 5
             self.lam_0 = 15
@@ -414,9 +451,19 @@ class MyApp(QDialog):
         self.x2000 = round((xgk_00 * m_00), 5)
         self.y2000 = round(((ygk_00 * m_00) + (self.strefa_00 * 1000000) + 500000), 5)
        
-    def XYZ(self):
-        if len(self.ui.lineEdit.text()) != 0:
-            self.hel = float(self.ui.lineEdit.text())
+    def XYZ(self,zmienna):
+        if zmienna == 'stopnie':
+            dlugosc_h = len(self.ui.lineEdit.text())
+            wartosc_h = float(self.ui.lineEdit.text())
+        if zmienna == 'grady':
+            dlugosc_h = len(self.ui.lineEdit_4.text())
+            wartosc_h = float(self.ui.lineEdit_4.text())
+        if zmienna == 'radiany':
+            dlugosc_h = len(self.ui.lineEdit_5.text())
+            wartosc_h = float(self.ui.lineEdit_5.text())
+            
+        if dlugosc_h != 0:
+            self.hel = wartosc_h
         else:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Critical)
